67225a1c9f2e3e3331c57d75338ebf901abd8d51
swiseman <swiseman10@gmail.com>
2022-01-02 14:07:58 -0500

    Update eval_e2e_out.bash

--- eval_e2e_out.bash
+++ eval_e2e_out.bash
@@ -1,4 +1,4 @@
 python strip_tags_and_deriv.py < $1 > $1.strpd
-python postprocess.py $1.strpd $1.pp
+python postprocess.py $1.strpd $1.pp # postprocess.py is from https://github.com/UFAL-DSG/tgen/blob/master/e2e-challenge/postprocess/postprocess.py
 python ../e2e-metrics/measure_scores.py -p $2 $1.pp
 
